d3fa24a861fdddbb344ff60891d3c38942ae458c
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-01-31 16:48:14 +1100

    new boss level challenge began

---
+++ jQuery/Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,9 @@
+var gamePattern = [];
+
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+var randomChosenColour = 
+
+function nextSequence() {
+    var randomNumber = Math.random() * 3;
+}
