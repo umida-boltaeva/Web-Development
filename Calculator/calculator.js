c7e5db46d7842dd0fad39cfec62b01f9418b3003
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-02-10 15:32:23 +1100

    here is bmi calculator!

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -4,20 +4,20 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
-});
-
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post("/", function(req, res){
+app.get("/bmiCalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
 
-    var num1 = Number(req.body.num1);
-    var num2 = Number(req.body.num2);
+app.post("/bmiCalculator", function(req, res){
 
-    var result = num1 + num2;
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
 
-    res.send("The result of the calculation is " + result);
+    var result = weight / (height * height);
+
+    res.send("Your BMI index is " + result);
 });
 
 app.get("/about", function(req, res){
