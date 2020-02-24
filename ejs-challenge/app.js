af08a0a1b702c3ba74b048175908cc77c39dc68c
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-02-24 14:50:33 +1100

    add contact page and about page to the blog

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -19,6 +19,14 @@ app.get("/", function(req, res){
   res.render("home", {startingContent: homeStartingContent});
 });
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutText: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactText: contactContent});
+});
+
 
 
 
