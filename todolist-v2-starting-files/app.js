7dd298cb85b91f562b0a02f7c5c4f1564639f3a8
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-03-11 12:25:54 +1100

    add new item to the todolist using mongoose

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -3,15 +3,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const date = require(__dirname + "/date.js");
-
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const itemsSchema = {
   name: {
@@ -20,28 +19,46 @@ const itemsSchema = {
   }
 };
 
-const Task = mongoose.model("item", itemsSchema);
+const Item = mongoose.model("Item", itemsSchema);
 
+const item1 = new Item ({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item ({
+  name: "Hit the + button to add a new item"
+});
+
+const item3 = new Item ({
+  name: "<-- Hit this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
+  Item.find({}, function(err, foundItems){
 
-  res.render("list", {listTitle: day, newListItems: items});
-
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems);
+      res.redirect("/");
+    }else{
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  });
 });
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+ const item = new Item ({
+   name: itemName
+ });
+
+// item.save();
+res.redirect("/");
+
 });
 
 app.get("/work", function(req,res){
